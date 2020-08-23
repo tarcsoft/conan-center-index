5e9544b566bea880eeb416f0da4fe20fc1710038
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-08-23 09:42:19 -0300

    Test

--- recipes/cln/all/test_package/conanfile.py
+++ recipes/cln/all/test_package/conanfile.py
@@ -1,6 +1,6 @@
 import os
-
 from conans import ConanFile, CMake, tools
+
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -12,9 +12,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            self.run("gdal_translate --formats", run_environment=True)
+        if not tools.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             self.run(bin_path, run_environment=True)
-            bin_path_c = os.path.join("bin", "test_package_c")
-            self.run(bin_path_c, run_environment=True)
